bd510ea541513282e5b191a32a979f7172d4e992
Castropy <randrescastroi@gmail.com>
2026-01-05 22:05:26 -0400

    RegistroComponent: Correcion del html

--- src/app/loggin/loggin.component.ts
+++ src/app/loggin/loggin.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';   
 
 @Component({
   selector: 'app-loggin',
   standalone: true,
+  imports: [RouterModule],                        
   templateUrl: './loggin.component.html',
   styleUrls: ['./loggin.component.css']
 })
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [RouterModule],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent { }
+export class RegistroComponent {}
