b6305728419ccc5aaaddb8ee8d952ac170125a29
Castropy <randrescastroi@gmail.com>
2026-01-18 20:33:43 -0400

    Agregando acceso denegado a la ruta de la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { Homepage } from './components/homepage/homepage.component';
+import { AccesoDenegado } from './components/acceso-denegado/acceso-denegado.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },       // ruta por defecto
   { path: 'registro', component: RegistroComponent }, // ruta para registro
-  { path: 'homepage', component: Homepage } // ruta para homepage
+  { path: 'homepage', component: Homepage }, // ruta para homepage
+  { path: 'acceso-denegado', component: AccesoDenegado }, //Ruta para acceso denegado
 ];
