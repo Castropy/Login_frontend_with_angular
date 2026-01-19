778d0373197371b5d098479bc76ff6fdb5a47e34
Castropy <randrescastroi@gmail.com>
2026-01-18 20:28:25 -0400

    Generando componente acceso denegado

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core'; // Necesario para usar servicios en funciones
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const token = localStorage.getItem('access_token'); // Buscamos el token de  Login
+
+  if (token) {
+    // Si existe el token, permitimos el paso a la Homepage
+    return true;
+  } else {
+    // Si NO existe, redirigimos al componente de aviso
+    // Podrías mandarlo directo a /login, pero querías mostrar botones primero
+    router.navigate(['/acceso-denegado']);
+    return false;
+  }
 };
