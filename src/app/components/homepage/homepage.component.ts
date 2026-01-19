4ae70d062dcad3aca359c7b3f67af8758a14515c
Castropy <randrescastroi@gmail.com>
2026-01-18 21:04:48 -0400

    Cerrar sesion + menu desplegable

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [], // Con @if no necesitas CommonModule
+  imports: [RouterModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css',
 })
 export class Homepage implements OnInit {
-  
-  // 1. Definimos una Signal con el valor inicial 'true'
+  private router = inject(Router);
+
   showWelcome = signal(true);
+  menuAbierto = signal(false);
 
   ngOnInit(): void {
     setTimeout(() => {
-      // 2. Cambiamos el valor de la Signal usando .set()
       this.showWelcome.set(false);
     }, 2000);
+  }
+
+  toggleMenu() {
+    this.menuAbierto.update(v => !v);
+  }
+
+  cerrarSesion() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    this.router.navigate(['/login']);
   }
 }
